02d7c672e69b2062168a9480ceee1aeaa5f4ba02
Edson Orivaldo Lessa Junior <84724309+edson-lessa-jr@users.noreply.github.com>
2022-11-08 20:41:38 -0300

    Atividade 1

--- src/atividade1/Matrix4x5.java
+++ src/atividade1/Matrix4x5.java
@@ -1,7 +1,24 @@
 package atividade1;
 
+import java.util.Random;
+
 public class Matrix4x5 {
     public static void main(String[] args) {
+        int matriz[][] = new int[4][5];
 
+        Random random = new Random(100);
+        for (int i = 0; i < 4; i++) {
+            for (int  j= 0;  j< 5; j++) {
+                int valor = random.nextInt(100);
+                matriz[i][j] = valor;
+                if (i==1){
+                    System.out.println("J= "+j+" - valor =  "+valor);
+                }
+            }
+        }
+        System.out.println("Resultado");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("coluna= "+i+" - valor =  "+matriz[1][i]);
+        }
     }
 }
