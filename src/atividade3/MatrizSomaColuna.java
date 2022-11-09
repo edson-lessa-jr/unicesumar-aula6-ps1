109cd94a01c0df1d8410e3addc2aa5c355d714ff
Edson Orivaldo Lessa Junior <84724309+edson-lessa-jr@users.noreply.github.com>
2022-11-08 21:10:09 -0300

    Atividade 3

--- src/atividade3/MatrizSomaColuna.java
+++ src/atividade3/MatrizSomaColuna.java
@@ -1,7 +1,27 @@
 package atividade3;
 
+import java.util.Random;
+
 public class MatrizSomaColuna {
     public static void main(String[] args) {
+        int matriz[][] = new int[6][7];
+        Random random = new Random();
+        int somatorio[] = new int[matriz[0].length];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = random.nextInt(10);
+                somatorio[j]+=matriz[i][j];
+            }
+        }
 
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j]+"  ");
+            }
+            System.out.println(" ");
+        }
+        for (int i = 0; i < somatorio.length; i++) {
+            System.out.print(somatorio[i]+" ");
+        }
     }
 }
